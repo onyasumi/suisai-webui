f1d56d53754eb195f79768e022f01b444ef81144
onyasumi <boku@kaneki.dev>
2023-09-08 17:32:28 -0400

    Started work on login page

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header class="bg-lime-600">
     <div class="header">
       <router-link class="link" to="/">
-        <h2 class="text-3xl font-bold underline">水彩</h2>
-        <h2>suisai</h2>
+        <h2 class="text-3xl text-lime-100">水彩</h2>
+        <h2 class="text-3xl text-stone-200">suisai</h2>
       </router-link>
     </div>
   </header>
 
   <body>
-    <!--<RouterView />-->
-    <div class=" container mx-auto py-10">
-      <div class="border m-6   bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-        <div class="sm:flex sm:items-center px-6 py-4">
-          <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://api.adorable.io/avatars/196/abott@adorable.png" alt="">
-          <div class="text-center sm:text-left sm:flex-grow">
-            <div class="mb-4">
-              <p class="text-xl leading-tight">Jane Doe</p>
-              <p class="text-sm leading-tight text-grey-dark">Software Developer at SpongeBob LLC.</p>
-            </div>
-            <div class="flex flex-wrap">
-              <button class=" text-xs font-semibold rounded-full px-4 py-1 mx-3  leading-normal bg-white border border-blue text-blue hover:bg-blue hover:text-white">Call</button>
-              <button class="  text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <RouterView />
   </body>
 </template>
 
@@ -50,15 +32,14 @@ import HelloWorld from './components/HelloWorld.vue'
   }
 
   header{
-
     width: 100%;
 
     div {
+      padding: 4px;
       font-size: 18px;
       width: 100%;
       display: flex;
       flex-direction: row;
-      background-color: orange;
     }
   }
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="w-full md:w-auto">
+      <form class="w-full m-20 max-w-sm">
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="email">
+              Email
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-lime-500" id="email" type="text" placeholder="akira@karatsubalabs.com">
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="password">
+              Password
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-lime-500" id="password" type="password" placeholder="********">
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="confirm-password">
+              Confirm Password
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-lime-500" id="confirm-password" type="password" placeholder="********">
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3"></div>
+          <label class="md:w-2/3 block text-gray-500 font-bold">
+            <input class="mr-2 leading-tight" type="checkbox">
+            <span class="text-sm">
+        Send me your newsletter!
+      </span>
+          </label>
+        </div>
+        <div class="md:flex md:items-center">
+          <div class="md:w-1/3"></div>
+          <div class="md:w-2/3">
+            <button class="shadow bg-lime-500 hover:bg-lime-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+  </main>
+</template>
